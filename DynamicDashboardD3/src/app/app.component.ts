368dab521f2c1441d8c92724631eafa024a6c4ae
Joel Rainear Wills <captainrdubb@outlook.com>
2017-07-01 07:56:26 -0500

    added styling to logo and added image url to component

--- DynamicDashboardD3/src/app/app.component.ts
+++ DynamicDashboardD3/src/app/app.component.ts
@@ -1,22 +1,25 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { IMenuItem } from './shared/interfaces';
 
 @Component({
   selector: 'dd-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements AfterViewInit {
   
   title = 'Dynamic Dashboard';
   logo = '../assets/logo.png';
-  
-  selectedItemId = -1;  
+  gravatarUrl = 'https://www.gravatar.com/avatar/36451f5c44ef99ae4d652b790763bfbd?s=100';
+
+  selectedItemId = 1;  
   menuItems = [
-    { id: 1, name: 'Dashboard' },
-    { id: 2, name: 'Human Resources' },
-    { id: 3, name: 'Messaging' },
+    { id: 1, name: 'Home' },
+    { id: 2, name: 'Dashboard' },
+    { id: 3, name: 'Grids/Search' },
+    { id: 4, name: 'Messaging' },
   ];
 
   constructor(){}
