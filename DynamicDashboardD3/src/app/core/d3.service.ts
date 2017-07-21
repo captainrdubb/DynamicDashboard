2e2e1c4a4b8d31c5290ea868b358aba4d0075ceb
Joel Rainear Wills <captainrdubb@outlook.com>
2017-07-21 06:37:27 -0500

    refactoring service

--- DynamicDashboardD3/src/app/core/d3.service.ts
+++ DynamicDashboardD3/src/app/core/d3.service.ts
@@ -7,27 +7,49 @@ export class D3Service {
 
   constructor() { }
 
-  createPieChart(svg: ElementRef, data: string[][], datumIndex: number, labelIndex: number): void {
+  createPieChart(svg: ElementRef, data: string[][], datumIndex: number, labelIndex: number, selectionCallback: (path: any, $event: Event) => void): void {
     var chartWidth = +(svg.nativeElement.clientWidth);
     var chartHeight = +(svg.nativeElement.clientHeight);
-    var radius = Math.min(chartWidth, chartHeight) / 2;
+    var radius = Math.min(chartWidth, chartHeight) / 2 - 3;
 
     let d3Svg = d3.select(svg.nativeElement);
-    var group = d3Svg.append("g");
-    group.attr("transform", "translate(" + (chartWidth / 2) + ',' + (chartHeight / 2) + ')');
 
-    var arc = d3.arc()
-      .outerRadius(radius)
-      .innerRadius(radius - (radius / 3));
+    let createGroup = (chartWidth, chartHeight) => {
+      var group = d3Svg.append("g");
+      group.attr("transform", "translate(" + (chartWidth / 2) + ',' + (chartHeight / 2) + ')')
+      return group;
+    }
 
+    let createText = (group: any) => {
+      let text = group.append("text")
+        .attr("text-anchor", "middle")
+        .attr("font-size", "20px")
+        .attr("fill", "white");
+      return text;
+    }
+
+    let createArc = (radius) => {
+      let arc = d3.arc()
+        .outerRadius(radius)
+        .innerRadius(radius - (radius / 3));
+      return arc;
+    }
+    
     var min = d3.min(data, (data) => {
       return +data[datumIndex];
     });
+
     var max = d3.max(data, (data) => {
       return +data[datumIndex];
     });
+
+    let group = createGroup(chartWidth, chartHeight);
+    let text = createText(group);
+    var arc = createArc(radius);
+    
     var color = d3.scaleSequential(d3Chromatic.interpolateSpectral).domain([0, data.length - 1]);
-    var scale = d3.scaleLinear()
+
+    var dataScale = d3.scaleLinear()
       .domain([min, max])
       .range([0, 100]);
 
@@ -36,26 +58,37 @@ export class D3Service {
       return a[labelIndex].localeCompare(b[labelIndex]);
     });
     pie.value((data) => {
-      return scale(+data[datumIndex]);
+      return dataScale(+data[datumIndex]);
     });
 
-    let difference = 0;
-    let multiplier = 2;
+    let format = d3.format(",");
     var path = group.selectAll("path")
       .data(pie(data))
       .enter()
       .append("path")
-      .on("click", () => {
-        d3.select(this)
-          .classed("selected", (d, i) => {
-            //return d3.select(this).classed("selected");
-          });
+      .on("click", (path: any) => {
+        let oldSpans = text.selectAll("tspan");
+        oldSpans.exit();
+        oldSpans.remove();
+
+        let labelSpan = text
+          .append("tspan")
+          .text(path.data[labelIndex])
+          .attr("x", "0")
+          .attr("dy", "-.5em");
+
+        let datumSpan = text
+          .append("tspan")
+          .text(format(+path.data[datumIndex]))
+          .attr("x", "0")
+          .attr("dy", "1.25em");
+
+        selectionCallback(path, d3.event);
       })
       .attr("class", "arc")
       .attr("d", arc)
       .attr("fill", function (d, i) {
         let paint = color(i);
-        console.log(paint);
         return paint;
       });
   }
--- DynamicDashboardD3/src/app/dashboard/widget/chart-widget/chart-widget.component.ts
+++ DynamicDashboardD3/src/app/dashboard/widget/chart-widget/chart-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ViewEncapsulation, Renderer } from '@angular/core';
 import { Response } from '@angular/http';
 
 import { DraggabillyDirective } from '../../../shared/draggabilly.directive';
@@ -13,9 +13,10 @@ import { D3Service } from '../../../core/d3.service';
 })
 export class ChartWidgetComponent implements AfterViewInit {
 
-  constructor(private censusDataService: CensusDataService, private d3Service: D3Service) { }
+  constructor(private censusDataService: CensusDataService, private d3Service: D3Service, private renderer: Renderer) { }
 
-  chartHeader = "Languages in Nebraska"
+  selectedPath: EventTarget;
+  chartHeader = "Languages in Nebraska";
   @ViewChild('chartWidget') chartWidget: ElementRef;
   @ViewChild(DraggabillyDirective) draggabillyDirective: DraggabillyDirective;
 
@@ -73,7 +74,13 @@ export class ChartWidgetComponent implements AfterViewInit {
     ["Omaha", "911", "300", "31"],
     ["Mayan languages", "968", "565", "31"]];
     data.shift();
-    this.d3Service.createPieChart(this.chartWidget, data, 2, 2);
+    this.d3Service.createPieChart(this.chartWidget, data, 2, 0, (path:any, event:Event) => {
+      if (this.selectedPath) {
+        this.renderer.setElementClass(this.selectedPath, "selected", false);
+      }
+      this.renderer.setElementClass(event.target, "selected", true);
+      this.selectedPath = event.target;
+    });
     this.draggabillyDirective.onItemsReady('.dashboard');
   }
 }
