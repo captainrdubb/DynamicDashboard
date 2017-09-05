a6ada18d51480a2ce5027288992b7c817702b768
rainearwillsj <joel.rainear-wills@fcsamerica.com>
2017-09-05 00:09:22 -0500

    cant forget note widget

---
+++ DynamicDashboardD3/src/app/dashboard/widget/note-widget/note-widget.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, AfterViewInit, ViewChild, Input } from '@angular/core';
+import { IWidgetComponent } from './../../../shared/interfaces';
+import { DraggabillyDirective } from '../../../shared/draggabilly.directive';
+
+
+@Component({
+  selector: 'dd-note-widget',
+  templateUrl: './note-widget.component.html',
+  styleUrls: ['./note-widget.component.scss']
+})
+export class NoteWidgetComponent implements AfterViewInit, IWidgetComponent {
+
+  editMode = false;
+  chartType: string;
+  data: string[][];
+  columnWidth: number;
+  destroy: () => void;
+  @Input() displayHeader;
+  @ViewChild(DraggabillyDirective) draggabillyDirective: DraggabillyDirective;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.draggabillyDirective.onItemsReady('.dashboard');
+  }
+
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+    if (this.editMode) {
+      this.draggabillyDirective.disable();
+    } else {
+      this.draggabillyDirective.enable();
+    }
+  }
+
+  onDestroyClick() {
+    this.destroy();
+  }
+}
